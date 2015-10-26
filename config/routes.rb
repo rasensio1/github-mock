2c845908524ae72d01e67a182558eda7515fb7aa
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-26 16:56:06 -0600

    add profile path

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/auth/github', as: :login
   get '/logout', to: "sessions#destroy"
   get '/auth/github/callback', to: "sessions#create"
+  get '/profile', to: "users#show"
 end

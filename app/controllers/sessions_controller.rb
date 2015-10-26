39ecad8b6e97e2da308a86fc8a9ba675f6242a65
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-26 14:32:49 -0600

    WIP add data to user model from github api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    if user = User.from_omniauth(request.env["omniauth.auth"]
+    if user = User.from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
     end
     redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
  def self.from_omniauth(auth_info)
+   byebug
     where(uid: auth_info[:uid]).first_or_create do |new_user|
+      auth_info.extra.raw_info.login
+
       new_user.uid                = auth_info.uid
       new_user.name               = auth_info.extra.raw_info.name
       new_user.screen_name        = auth_info.extra.raw_info.screen_name

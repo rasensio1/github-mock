290656f982f43c40890b82997092b554845a8429
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-26 14:41:24 -0600

    adds stores for github oauth params

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,16 @@ class User < ActiveRecord::Base
  def self.from_omniauth(auth_info)
    byebug
     where(uid: auth_info[:uid]).first_or_create do |new_user|
-      auth_info.extra.raw_info.login
-
       new_user.uid                = auth_info.uid
-      new_user.name               = auth_info.extra.raw_info.name
-      new_user.screen_name        = auth_info.extra.raw_info.screen_name
-      new_user.oauth_token        = auth_info.credentials.token
-      new_user.oauth_token_secret = auth_info.credentials.secret
+      new_user.name               = better_info.extra.raw_info.name
+      new_user.screen_name        = better_info.extra.raw_info.login
+      new_user.oauth_token        = better_info.credentials.token
+      #new_user.oauth_token_secret = better_info.credentials.secret
     end
  end 
+
+ private
+  def better_info(auth_info)
+    auth_info.extra.raw_info
+  end
 end

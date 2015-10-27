0920af3b94738dc053b1ea7e82d7614b9969ffe2
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-26 18:01:39 -0600

    add layout for user profile/show

--- test/integration/user_views_profile_test.rb
+++ test/integration/user_views_profile_test.rb
@@ -8,7 +8,11 @@ include Capybara::DSL
     click_link "Profile"
 
     assert_equal profile_path, current_path
-    assert page.has_content?("Profile")
+    assert page.has_content?("Ryan A")
+    assert page.has_content?("rasensio1")
+    assert page.has_content?("Contribution Summary")
+    assert page.has_content?("Organizations")
+    assert page.has_content?("Starred Repos")
   end
 
 end

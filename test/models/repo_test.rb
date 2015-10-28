ecece60267def29cd2b640527a88dbd6d075fdb9
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-28 13:32:31 -0600

    add test for repo starred method

--- test/models/repo_test.rb
+++ test/models/repo_test.rb
@@ -5,7 +5,7 @@ include Capybara::DSL
 
 fixtures :all
 
-  test "viewing profile layout" do
+  test "get repos for a user" do
     VCR.use_cassette("repo#mine") do
       repos = Repo.mine(users(:ryan))
       repo = repos.first
@@ -13,6 +13,17 @@ fixtures :all
       assert_equal 30, repos.count
       assert_equal "active-record-sinatra", repo[:name]
       assert_equal "https://github.com/rasensio1/active-record-sinatra", repo[:url]
+    end
+  end
+
+  test "get starred repos for a user" do
+    VCR.use_cassette("repo#starred") do
+      repos = Repo.starred(users(:ryan))
+      repo = repos.first
+
+      assert_equal 6, repos.count
+      assert_equal "curriculum", repo[:name]
+      assert_equal "https://github.com/turingschool/curriculum", repo[:url]
     end
   end
 end

d5d7f6ce98d3e72d073a57f504b0e5c2de73064d
Ryan Asensio <ryan.asensio@gmail.com>
2015-10-28 11:16:13 -0600

    add vcr cassettes

--- test/integration/user_views_profile_test.rb
+++ test/integration/user_views_profile_test.rb
@@ -4,29 +4,32 @@ class UserViewsProfileTest < ActionDispatch::IntegrationTest
 include Capybara::DSL
 
   test "viewing profile layout" do
-    log_in
-    click_link "Profile"
+    VCR.use_cassette("profile layout") do
+      log_in
+      click_link "Profile"
 
-    assert_equal profile_path, current_path
-    assert page.has_content?("Ryan A")
-    assert page.has_content?("rasensio1")
-    assert page.has_content?("Contribution Summary")
-    assert page.has_content?("Organizations")
-    assert page.has_content?("Starred Repos")
+      assert_equal profile_path, current_path
+      assert page.has_content?("Ryan A")
+      assert page.has_content?("rasensio1")
+      assert page.has_content?("Contribution Summary")
+      assert page.has_content?("Organizations")
+      assert page.has_content?("Starred Repos")
+    end
   end
 
   test "viewing stored data" do
-    log_in
-    visit "/profile"
+    VCR.use_cassette("profile data") do
+      log_in
+      visit "/profile"
 
-    assert page.has_content?("3")
-    assert page.has_content?("6")
-    assert page.has_content?("ohyeah.jpg")
-  end
-
-  test "list organizations" do
-    log_in
-    visit "/profile"
+      assert page.has_content?("3")
+      assert page.has_content?("6")
+      assert page.has_content?("Organizations")
+      assert page.has_content?("Starred")
+      assert page.has_content?("Contribution Summary")
+      assert page.has_content?("My Activity")
+      assert page.has_content?("Following Activity")
+    end
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'webmock'
+require 'vcr'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  VCR.configure  do |c|
+    c.cassette_library_dir = 'test/cassettes'
+    c.hook_into :webmock
+  end
+
   def setup
     Capybara.app = GithubMock::Application
     stub_omniauth
@@ -36,6 +40,5 @@ class ActiveSupport::TestCase
       info: {nickname: "rasensio1"}
     })
   end
-
 
 end

39561229d4f3cfc047d99802d9fccf1e291bed5f
Ryan Asensio <Ryan.asensio@gmail.com>
2015-10-29 11:08:19 -0600

    Update togglehide.js

--- app/assets/javascripts/togglehide.js
+++ app/assets/javascripts/togglehide.js
@@ -1,8 +1,7 @@
 $( document ).ready(function() {
-
   $('#following-btn').click(function(){
     $('#following').slideToggle();
-  })
+  });
 
   $('#my-events-btn').click(function(){
     $('#my-events').slideToggle();
